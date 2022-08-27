f4ea0d7f3d9a43edf01e2213a29e15cfced51cd2
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 15:19:35 +0530

    Added PalindromeNumber class

---
+++ PalindromeNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassPractise
+{
+    public class PalindromeNumber
+    {
+        public static void CheckNumber()
+        {
+            try
+            {
+                Console.Write("\n Input a number : "); var input = Console.ReadLine();
+                var reversed = "";
+
+                for (var i = 1; i <= input.Length; i++)
+                {
+                    reversed += input[input.Length - i];
+                }
+
+                if (input == reversed)
+                    Console.WriteLine("\n >>>> {0} is a Palindrome", input);
+                else
+                    Console.WriteLine("\n >>>> {0} is NOT a Palindrome", input);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n !!Try Again!!");
+                CheckNumber();
+            }
+        }
+    }
+}

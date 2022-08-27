1db9ca7fe5e0a5dadcf4a627c37ac35ce1cb0102
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 15:19:13 +0530

    Added Case class

---
+++ Case.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassPractise
+{
+    public class Case
+    {
+        public static void CheckCase()
+        {
+            try
+            {
+                Console.Write("\nEnter a string : "); var input = Console.ReadLine();
+
+                var upper = "";
+                var lower = "";
+
+                foreach (var alphabet in input)
+                {
+                    if (alphabet == char.ToUpper(alphabet))
+                        upper += alphabet;
+                    else
+                        lower += alphabet;
+                }
+                Console.WriteLine("\n >>>> Upper case characters : {0}, Lower case characters : {1} \n", upper, lower);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n !!Try Again !! \n");
+                CheckCase();
+            }
+        }
+    }
+}

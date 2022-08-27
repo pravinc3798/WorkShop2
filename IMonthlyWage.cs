6324aa4875bac75f183d63e591f4a49d94b5930a
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 15:19:29 +0530

    Added IMonthlyWage class

---
+++ IMonthlyWage.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassPractise
+{
+    //Calculate the different company monthly hrs daily wage is 100 part time 4hrs and full time 8hrs interface classs with arraylist//
+    public interface IMonthlyWage
+    {
+        public void AddCompany();
+        public void GetWage(string companyName);
+    }
+}

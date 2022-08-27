385a37717939834072cb7baa72057de820c9adf5
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-27 15:19:08 +0530

    Added ArmstrongNumber class

---
+++ ArmstrongNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassPractise
+{
+    public class ArmstrongNumber
+    {
+        public static void CheckNumber()
+        {
+            try
+            {
+                Console.Write("\n Input a number : "); var input = Console.ReadLine();
+                
+                var sum = 0;
+                for(int i = 0; i < input.Length; i++)   
+                {
+                    var number = (int) Char.GetNumericValue(input[i]);
+                    sum += number * number * number;
+                }
+
+                if (Convert.ToInt32(input) == sum)
+                    Console.WriteLine("\n >>>> {0} is a ArmStrong Number", input);
+                else
+                    Console.WriteLine("\n >>>> {0} is NOT a ArmStrong Number", input);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n !!Try Again!!");
+                CheckNumber();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace ClassPractise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var company = new CompanyWage();
+            Console.Write(" ----------- Practise Problems --------- \n 1. Swap Numbers \n 2. Check Case \n 3. Check Palindrome \n 4. Check Armstrong Number \n 5. Company Wise Wages \n 6. Exit \n Enter the number corresponding to required program : "); var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1": Swap.GetSwapedNumber(); Program.Main(args); break;
+                case "2": Case.CheckCase(); Program.Main(args); break;
+                case "3": PalindromeNumber.CheckNumber(); Program.Main(args); break;
+                case "4": ArmstrongNumber.CheckNumber(); Program.Main(args); break;
+                case "5":
+                    {
+                        company.AddCompany();
+                        Console.WriteLine("Company Added !!");
+                        Console.Write("Enter Company Name : "); var cname = Console.ReadLine();
+                        company.GetWage(cname.ToUpper());
+                        Program.Main(args);
+                        break;
+                    }
+                case "6": break;
+                default: Console.WriteLine("\n Try Again !!\n"); Program.Main(args); break;
+            }
+        }
+    }
+}
